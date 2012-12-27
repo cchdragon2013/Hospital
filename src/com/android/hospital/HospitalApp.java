5690d1ca525f6cb5aa6869dadc65beeba8ffbb95
Eleven <Eleven@Eleven-PC>
2012-12-27 21:24:49 +0800

    1234

--- src/com/android/hospital/HospitalApp.java
+++ src/com/android/hospital/HospitalApp.java
@@ -13,7 +13,7 @@ import android.app.Application;
 /**
  * 
 * @ClassName: HospitalApp 
-* @Description: TODO(æ≤Ã¨±‰¡ø) 
+* @Description: 
 * @author wanghailong 81813780@qq.com 
 * @date 2012-12-18 …œŒÁ8:57:34 
 *
