1bf1a61796929ea976b777e101ce7db59e0e5025
Eleven <Eleven@Eleven-PC>
2012-12-28 14:50:23 +0800

    kjhkj

--- src/com/android/hospital/ui/fragment/AddPrescriptionFragment.java
+++ src/com/android/hospital/ui/fragment/AddPrescriptionFragment.java
@@ -454,10 +454,11 @@ public class AddPrescriptionFragment extends BaseFragment implements OnClickList
 			buffer.append("'"+itemEntity.administration).append("',");
 			buffer.append("'"+itemEntity.dosage).append("',");
 			buffer.append("'"+itemEntity.dose_units).append("',");
+			buffer.append("'"+"").append("',");
 			buffer.append("'"+itemEntity.frequency).append("',");
 			buffer.append("'"+itemEntity.dosage_each).append("',");
 			buffer.append("'"+itemEntity.freq_detail).append("',");
-			buffer.append("'"+itemEntity.is_basic.trim()).append("',");
+			buffer.append("'"+itemEntity.is_basic.trim()).append("')");
 			WebServiceHelper.insertWebServiceData(buffer.toString());
 		}
 	}
